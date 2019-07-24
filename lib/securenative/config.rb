fecb7db28fc33b8af4ac139272523ff00b2af953
Inbal Tako <inbal@securenative.com>
2019-07-24 15:47:14 +0300

    Add config

--- lib/securenative/config.rb
+++ lib/securenative/config.rb
@@ -1,2 +1,6 @@
-class ConfigManager
+class Config
+  @sdk_version = '0.1.5'
+  @max_allowed_params = 6
+  @securenative_prod = u"https://api.securenative.com"
+  @securenative_stg = u"https://api.securenative-stg.com"
 end
