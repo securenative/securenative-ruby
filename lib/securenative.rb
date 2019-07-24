2a44856ca0254c0d2deeb032a4b6e598e9b24b80
Inbal Tako <inbal@securenative.com>
2019-07-24 15:46:54 +0300

    Restructure project

--- lib/securenative.rb
+++ lib/securenative.rb
@@ -1,2 +1,24 @@
 class SecureNative
+  def initialize(api_key, options=SecureNativeOptions())
+    if api_key == nil
+      raise ArgumentError.new('API key cannot be None, please get your API key from SecureNative console.')
+    end
+
+    @api_key = api_key
+    @options = options
+    @event_manager = EventManager(@api_key, @options)
+  end
+
+  def track(event)
+  end
+
+  def verify(event)
+  end
+
+  def flow(event)
+  end
+
+  private
+  def validate_event(event)
+  end
 end
