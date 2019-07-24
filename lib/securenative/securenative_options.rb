8176e16b07a4f0423b80d5ee42edd045075ee9c1
Inbal Tako <inbal@securenative.com>
2019-07-24 15:48:01 +0300

    Add sn options

--- lib/securenative/securenative_options.rb
+++ lib/securenative/securenative_options.rb
@@ -1,2 +1,11 @@
-class SDKOptions
+require 'config.rb'
+
+class SecureNativeOptions
+  def initialize(api_url=Config.securenative_prod, interval=1000, max_events=1000, timeout=1500, auto_send=true)
+    @timeout = timeout
+    @max_events = max_events
+    @api_url = api_url
+    @interval = interval
+    @auto_send = auto_send
+  end
 end
