25195a587a1b91896a39e425fdf5f938a4c750c1
Inbal Tako <inbal@securenative.com>
2019-07-25 15:11:58 +0300

    Add utils

--- lib/securenative/utils.rb
+++ lib/securenative/utils.rb
@@ -1,2 +1,42 @@
+require "base64"
+require "json"
+
 module Utils
+  def verify_signature(secret, text_body, header_signature)
+    begin
+      key = secret.encode('utf-8')
+      body = text_body.encode('utf-8')
+      calculated_signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha512'), key, body)
+      calculated_signature.eql? header_signature
+    rescue Exception
+      return false
+    end
+  end
+
+  private
+
+  def parse_cookie(cookie = nil)
+    fp = ""
+    cid = ""
+    unless cookie
+      return fp, cid
+    end
+
+    begin
+      decoded_cookie = Base64.decode64(cookie)
+      unless decoded_cookie
+        decoded_cookie = "{}"
+      end
+      jsonified = JSON.generate(decoded_cookie)
+      if jsonified["fp"]
+        fp = jsonified["fp"]
+      end
+      if jsonified["cid"]
+        cid = jsonified["cid"]
+      end
+    rescue Exception
+    ensure
+    return fp, cid
+    end
+  end
 end
