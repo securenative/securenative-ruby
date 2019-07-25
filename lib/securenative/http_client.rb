2fc238d85258d9a8f61f7e258605c205686f4c94
Inbal Tako <inbal@securenative.com>
2019-07-25 15:55:36 +0300

    Add http client

--- lib/securenative/http_client.rb
+++ lib/securenative/http_client.rb
@@ -1,7 +1,16 @@
 module HTTPClient
-  def initialize
+  @client = HTTPClient.new
+
+  def headers(api_key)
+    {
+        "Content-Type" => 'application/json',
+        "User-Agent" => 'SecureNative-python',
+        "Sn-Version" => Config.sdk_version,
+        "Authorization" => api_key
+    }
   end
 
-  def post
+  def post(url, api_key, body)
+    @client.post(url, body, self.headers(api_key))
   end
 end
