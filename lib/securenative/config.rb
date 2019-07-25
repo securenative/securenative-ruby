eaf533d927f565cb4bebb8a6b9e8dc34f728325d
Inbal Tako <inbal@securenative.com>
2019-07-25 14:35:54 +0300

    Add event options

--- lib/securenative/config.rb
+++ lib/securenative/config.rb
@@ -1,8 +1,8 @@
 module Config
   @sdk_version = '0.1.5'
   @max_allowed_params = 6
-  @securenative_prod = u "https://api.securenative.com"
-  @securenative_stg = u "https://api.securenative-stg.com"
-  @track_event = u "collector/api/v1/track"
-  @verify_event = u "collector/api/v1/verify"
+  @securenative_prod = "https://api.securenative.com"
+  @securenative_stg = "https://api.securenative-stg.com"
+  @track_event = "collector/api/v1/track"
+  @verify_event = "collector/api/v1/verify"
 end
