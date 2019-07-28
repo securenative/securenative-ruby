0c8692479930d03dc102fede87dc58d75ed1631e
Inbal Tako <inbal@securenative.com>
2019-07-28 17:17:32 +0300

    Add integration tests

---
+++ test/test_sdk.rb
@@ -0,0 +1,35 @@
+require_relative '../lib/securenative'
+require_relative '../lib/securenative/securenative_options'
+require_relative '../lib/securenative/event_builder'
+require_relative '../lib/securenative/event_type'
+require 'rspec/autorun'
+require 'securerandom'
+require 'json'
+
+describe SecureNative do
+  let(:sn_options) {SecureNativeOptions.new}
+  let(:sn_client) {SecureNative.new("5B0A332762FD61C3B74818C9580F6F31ECE1803F", sn_options)}
+  let(:event_builder) {EventBuilder.new}
+
+  it "use sdk without api key" do
+    api_key = nil
+    expect {SecureNative.new(api_key, sn_options)}.to raise_error(ArgumentError)
+  end
+
+  it "track an event" do
+    user_id = SecureRandom.uuid
+    event = event_builder.build(EventType::LOG_IN, user_id)
+    expect(sn_client.track(event)).not_to be_empty
+  end
+
+  it "verify an event" do
+    user_id = SecureRandom.uuid
+    event = event_builder.build(EventType::LOG_OUT, user_id)
+    res = sn_client.verify(event)
+
+    expect(res).not_to be_empty
+    expect(res['triggers']).not_to be_empty
+    expect(res['riskLevel']).to eq('high')
+    expect(res['score']).to eq(0.64)
+  end
+end
