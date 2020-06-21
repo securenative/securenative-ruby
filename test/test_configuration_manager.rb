b49e5d3f5654a47305257c245d8bca9ba20ebcf3
Inbal Tako <inbal@securenative.com>
2020-06-21 11:07:32 +0300

    Add unit tests

--- test/test_configuration_manager.rb
+++ test/test_configuration_manager.rb
@@ -1,3 +1,29 @@
 describe ConfigurationManager do
+  it "parses a config file correctly" do
+    # code here
+  end
 
+  it "ignores unknown config in properties file" do
+    # code here
+  end
+
+  it "handles invalid config file" do
+    # code here
+  end
+
+  it "ignores invalid config file entries" do
+    # code here
+  end
+
+  it "loads default config" do
+    # code here
+  end
+
+  it "gets config from env variables" do
+    # code here
+  end
+
+  it "uses default values for invalid enum config props" do
+    # code here
+  end
 end
