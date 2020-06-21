4a9f94f0129f2c863d4fe9ea8e33774753248f09
Inbal Tako <inbal@securenative.com>
2020-06-21 15:10:32 +0300

    Fix project structure

---
+++ spec/securanative_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Securanative do
+  it "has a version number" do
+    expect(Securanative::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require "bundler/setup"
+require "securenative/lib/securenative/securenative"
+
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
