96e654bd1cb129f7f6b65450f03d38b7d1991721
Inbal Tako <inbal@securenative.com>
2020-08-18 12:08:53 +0300

    Fix codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'simplecov'
 require 'codecov'
+
+SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
