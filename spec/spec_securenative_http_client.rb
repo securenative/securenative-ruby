dc8bc200ab47477533a48060aaa8008d120e17f1
Inbal Tako <inbal@securenative.com>
2020-09-24 16:19:19 +0300

    Bump version

--- spec/spec_securenative_http_client.rb
+++ spec/spec_securenative_http_client.rb
@@ -16,7 +16,7 @@ RSpec.describe SecureNative::SecureNativeHttpClient do
               'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
               'Authorization' => 'YOUR_API_KEY',
               'Content-Type' => 'application/json',
-              'Sn-Version' => '0.1.24',
+              'Sn-Version' => '0.1.25',
               'User-Agent' => 'SecureNative-ruby'
             }).to_return(status: 200, body: '', headers: {})
 
