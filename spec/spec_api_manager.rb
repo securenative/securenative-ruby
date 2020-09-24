16c47670acc12a094584326b1a5877614c7e7e66
Inbal Tako <inbal@securenative.com>
2020-09-24 16:15:17 +0300

    Bump version

--- spec/spec_api_manager.rb
+++ spec/spec_api_manager.rb
@@ -63,7 +63,7 @@ RSpec.describe SecureNative::ApiManager do
         'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'Authorization' => 'YOUR_API_KEY',
         'Content-Type' => 'application/json',
-        'Sn-Version' => '0.1.23',
+        'Sn-Version' => '0.1.24',
         'User-Agent' => 'SecureNative-ruby'
       }
     ).to_return(status: 200, body: '', headers: {})
