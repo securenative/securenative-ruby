ec2c953fccf64edaabe1b78a56e5a281a8ff45f3
Inbal Tako <inbal@securenative.com>
2020-10-13 17:07:37 +0300

    Fix request headers structure

--- spec/securenative/spec_api_manager.rb
+++ spec/securenative/spec_api_manager.rb
@@ -55,7 +55,7 @@ RSpec.describe SecureNative::ApiManager do
         'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'Authorization' => 'YOUR_API_KEY',
         'Content-Type' => 'application/json',
-        'Sn-Version' => '0.1.34',
+        'Sn-Version' => '0.1.35',
         'User-Agent' => 'SecureNative-ruby'
       }
     ).to_return(status: 200, body: '', headers: {})
